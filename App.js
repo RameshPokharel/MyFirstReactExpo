395af575cb4bac8819d0e00e0a4603515336ed85
Ramesh Pokhrel <kanxoramesh@gmail.com>
2021-05-24 11:42:21 +0545

    implement naviagation

--- App.js
+++ App.js
@@ -2,22 +2,17 @@ import * as React from 'react'
 import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  )
-}
+import SearchScreen from './src/screens/SearchScreen'
+import HomeScreen from './src/screens/HomeScreen'
 
 const Stack = createStackNavigator()
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Home``'>
         <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Search' component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
